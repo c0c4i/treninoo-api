273f66d4633c355a832afcf2d837d6d807eb7240
Samuele Besoli <samuele.besoli.sb@gmail.com>
2023-04-22 00:10:56 +0200

    update build settings

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker build --platform linux/amd64 -t treninoo-api:$1 .
 docker tag treninoo-api:$1 c0c4i/treninoo-api:$1
 
 # Push new container to DockerHub repository
-docker push c0c4i/treninoo-api:$1
+# docker push c0c4i/treninoo-api:$1
