9ddbde6cdd1eb7d2c0d1f4e7186a5d99cb5d777d
Samuele Besoli <samuele.besoli.sb@gmail.com>
2023-04-21 23:31:04 +0200

    improve build steps

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-rm -rf ./build
-yarn build
-cd build
-cp ../.env ./.env
-yarn install --production
-node server.js
+# Build and tag containers
+docker build --platform linux/amd64 -t treninoo-api:$1 .
+docker tag treninoo-api:$1 c0c4i/treninoo-api:$1
+
+# Push new container to DockerHub repository
+docker push c0c4i/treninoo-api:$1
