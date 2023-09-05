ea340d0d2e36bc5b17935cf50a88c50f5bdefe86
Samuele Besoli <samuele.besoli.sb@gmail.com>
2023-09-05 13:14:27 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,6 +1,2 @@
-# Build and tag containers
-docker build --platform linux/amd64 -t treninoo-api:$1 .
-docker tag treninoo-api:$1 c0c4i/treninoo-api:$1
-
-# Push new container to DockerHub repository
-# docker push c0c4i/treninoo-api:$1
+# Build and push new container to DockerHub repository
+docker buildx build --push --platform linux/amd64,linux/arm64 -t c0c4i/treninoo-api:$1 .
