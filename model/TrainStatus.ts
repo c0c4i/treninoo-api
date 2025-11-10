ae50cd7a516175d7d137c175bcd83313b8c80e63
Samuele Besoli <samuele.besoli.sb@gmail.com>
2025-11-10 18:56:52 +0100

    fix: update isDeparted determination logic to ensure proper null check for lastDetectionTime

--- model/TrainStatus.ts
+++ model/TrainStatus.ts
@@ -28,7 +28,7 @@ class TrainStatus {
     // Determine if the train has departed
     const lastDetectionStation = json.stazioneUltimoRilevamento
     const lastDetectionTime = json.oraUltimoRilevamento
-    const isDeparted = lastDetectionStation !== '--' && lastDetectionTime
+    const isDeparted = lastDetectionStation !== '--' && lastDetectionTime != null
 
     return new TrainStatus({
       trainType: this._parseCategory(json.compNumeroTreno),
